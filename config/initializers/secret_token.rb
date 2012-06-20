9efcf6579a0cd571b910662154bea155c7dd2c7c
Josh Starcher <josh.starcher@gmail.com>
2012-06-20 17:38:08 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OracleFeeds::Application.config.secret_token = '2babb530ef0563ea35fe91f58c1c359833d18c60c492519d55c2bef0e5aad04b256b02c6d931e51893b201b82c0c2fbbce57566493da74f932b703e4232cd121'
