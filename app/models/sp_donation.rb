e174acd9b9d22dfaf593fe7fd5c807f19e8913a2
Josh Starcher <josh.starcher@gmail.com>
2012-07-10 08:34:37 -0400

    improve efficiency of donation import

--- app/models/sp_donation.rb
+++ app/models/sp_donation.rb
@@ -1,21 +1,23 @@
 class SpDonation < ActiveRecord::Base
 
   def self.update_from_peoplesoft
-    # last_date = SpDonation.maximum(:donation_date) || 2.years.ago
     rows = PsDonation.connection.select_all("select * from hrsdon.ps_student_load_vw")
-    SpDonation.delete_all(["donation_date >  ?", 1.year.ago])
-    SpDonation.transaction do
-      rows.each do |row|
-        row[:designation_number] = row.delete('designation')
-        row[:donor_name] = row.delete('acct_name')
-        row[:medium_type] = row.delete('don_medium_type')
-        if donation = SpDonation.find_by_donation_id(row['donation_id'])
-          donation.update_attributes(row)
-        else
-          SpDonation.create(row)
-        end
+    donation_ids = []
+    rows.each do |row|
+      row[:designation_number] = row.delete('designation')
+      row[:donor_name] = row.delete('acct_name')
+      row[:medium_type] = row.delete('don_medium_type')
+      if donation = SpDonation.find_by_donation_id(row['donation_id'])
+        donation.update_attributes(row)
+      else
+        SpDonation.create(row)
       end
+      donation_ids << row['donation_id']
     end
+
+    # Delete any donations not found in this dump
+    SpDonation.delete_all(["donation_date >  ? and donation_id not in (?)", 1.year.ago, donation_ids])
+
     rows.length
   end
 
